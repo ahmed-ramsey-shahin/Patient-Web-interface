8ef3c4988056b45fa2d6e3ba3c022d6e4327a3b7
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2025-07-30 15:10:08 +0300

    adding screenshots

--- terraform/3-main.tf
+++ terraform/3-main.tf
@@ -6,6 +6,21 @@ module "vpc" {
   private_subnet_cidr = var.private_subnet_cidr
   availability_zones = var.availability_zones
 }
+
+module "eks" {
+    source = "./modules/eks"
+    cluster_name = var.cluster_name
+    admin_acess = var.admin_acess
+    cluster_version = var.cluster_version
+    private_subnet_ids = module.vpc.private_subnet_ids
+    public_subnet_ids = module.vpc.public_subnet_ids
+    max_size = var.max_size
+    min_size = var.min_size
+    desired_size = var.desired_size
+    instance_types = var.instance_types
+    capacity_type = var.capacity_type
+}
+
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2-sg"
   description = "Allow SSH"
